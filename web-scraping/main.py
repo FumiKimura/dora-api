ac2affb064e97630521f7370808d4ed91c33778d
Norifumi Kimura <norical496@gmail.com>
2021-12-28 22:13:37 +0900

    Implemented to move to detail page and grab gadget name

--- web-scraping/main.py
+++ web-scraping/main.py
@@ -1,21 +1,41 @@
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
+from bs4 import BeautifulSoup
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
 DRIVER_PATH = os.environ.get("DRIVER_PATH")
-print(DRIVER_PATH)
-
+STARTING_URL = "https://doraemon.fandom.com/wiki/Category:Gadgets"
 options = Options()
 options.headless = True
 options.add_argument("--window-size=1920, 1200")
-
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
-driver.get("https://doraemon.fandom.com/wiki/Category:Gadgets")
-all_titles = driver.find_elements_by_class_name("category-page__member-link")
 
-for title in all_titles:
-    print(title.text)
 
+def getGadgetList(URL):
+    try:
+        driver.get(URL)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        gadgetList = soup.find_all(
+            'a', attrs={'class': 'category-page__member-link'})
+
+        for gadget in gadgetList:
+            detailPageURL = "https://doraemon.fandom.com" + gadget.get('href')
+
+            getDetailPageInfo(detailPageURL, driver)
+
+    except Exception as e:
+        print(e)
+
+
+def getDetailPageInfo(URL, driver):
+    driver.get(URL)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    name = soup.find(attrs={'id': 'firstHeading'}).text.strip()
+    print(name)
+    driver.back()
+
+
+getGadgetList(STARTING_URL, driver)
 driver.quit()
