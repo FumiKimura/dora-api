9f2f322ff8a5304a9d9c1ae03436abdd8fa28722
Norifumi Kimura <norical496@gmail.com>
2021-12-30 14:56:24 +0900

    Changed breaking loop from try and catch to if statement

--- web-scraping/main.py
+++ web-scraping/main.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import time
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -15,32 +16,33 @@ driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
 
 def getGadgetList(URL):
     while True:
-        try:
-            driver.get(URL)
-            print(URL)
-            soup = BeautifulSoup(driver.page_source, 'html.parser')
-            gadgetList = soup.find_all(
-                'a', attrs={'class': 'category-page__member-link'})
+        driver.get(URL)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        gadgetList = soup.find_all(
+            'a', attrs={'class': 'category-page__member-link'})
 
-            for gadget in gadgetList:
-                detailPageURL = "https://doraemon.fandom.com" + \
-                    gadget.get('href')
+        for gadget in gadgetList:
+            detailPageURL = "https://doraemon.fandom.com" + \
+                gadget.get('href')
 
-                # getDetailPageInfo(detailPageURL)
-
-            nextButton = soup.find('a', attrs={
-                'class': 'category-page__pagination-next wds-button wds-is-secondary'})
-            URL = nextButton.get('href')
-        except Exception as e:
-            print('Finished')
+            getDetailPageInfo(detailPageURL)
             break
+
+        nextButton = soup.find('a', attrs={
+            'class': 'category-page__pagination-next wds-button wds-is-secondary'})
+
+        if nextButton is None:
+            print("Finished")
+            break
+
+        URL = nextButton.get('href')
 
 
 def getDetailPageInfo(URL):
+    time.sleep(3)
     driver.get(URL)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     name = soup.find(attrs={'id': 'firstHeading'}).text.strip()
-    print(name)
     driver.back()
 
 
